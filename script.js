f4bbe7606ab4edd6719cfc76968862ef518b0648
yunjing23 <cschuster@tenbagroup.com>
2022-07-27 11:11:02 +0300

    Add keydown functionality

--- script.js
+++ script.js
@@ -1,12 +1,81 @@
 const add=(a,b)=>a+b;
 const subtract=(a,b)=>a-b;
-const multiply=(a,b)=>a*b;
-const divide=(a,b)=>a/b;
+const multiply=(a,b)=>Math.round(a*b);
+const divide=(a,b)=>Math.round(a/b);
 const operate=(operator,a,b)=>{
     switch(operator){
-        case "add":return add(a,b);
-        case "sub":return subtract(a,b);
-        case "div":return divide(a,b);
-        case "mul":return multiply(a,b);
+        case "+":return add(a,b);
+        case "-":return subtract(a,b);
+        case "/":return divide(a,b);
+        case "*":return multiply(a,b);
     }
 };
+const clearAll=()=>{
+    prevValue="";
+    clickedOp="";
+    total="";
+    clickedOpTimes=false;
+    document.querySelector(".display").innerText="";
+    document.querySelector(".old").innerText="";
+    document.querySelector(".operator").innerText="";
+};
+
+let clickedOp="";
+let prevValue="";
+let total="";
+let clickedOpTimes=false;
+
+//Numbers Buttons
+document.querySelectorAll(".numbers button").forEach(elem=>elem.addEventListener("click",elem=>{
+    if(!clickedOpTimes){document.querySelector(".display").innerText+=elem.target.innerText;}
+    else{document.querySelector(".display").innerText=elem.target.innerText;}
+    clickedOpTimes=false;
+    console.log(prevValue);
+}));
+
+//Function Buttons
+document.querySelectorAll(".op,.eq").forEach(elem=>elem.addEventListener("click",elem=>
+{
+   //console.log(total);
+   clickedOpTimes=true;
+   prevValue=document.querySelector(".display").innerText;
+   
+   if(document.querySelector(".old").innerText==""){document.querySelector(".old").innerText=prevValue;}
+   else{
+    if(document.querySelector(".display").innerText!=""){
+    switch (clickedOp){
+        case "+":
+            total=operate(clickedOp,parseInt(document.querySelector(".old").innerText),parseInt(document.querySelector(".display").innerText));document.querySelector(".old").innerText=total;clickedOpTimes=true;break;
+        case "*":
+            total=operate(clickedOp,parseInt(document.querySelector(".old").innerText),parseInt(document.querySelector(".display").innerText));document.querySelector(".old").innerText=total;clickedOpTimes=true;break;
+        case "/":
+            total=operate(clickedOp,parseInt(document.querySelector(".old").innerText),parseInt(document.querySelector(".display").innerText));if(total==Infinity){alert("Error, don't divide by 0!");clearAll();}else{document.querySelector(".old").innerText=total;}clickedOpTimes=true;break;
+        case "-":
+            total=operate(clickedOp,parseInt(document.querySelector(".old").innerText),parseInt(document.querySelector(".display").innerText));document.querySelector(".old").innerText=total;clickedOpTimes=true;break;
+    }
+   }}
+   document.querySelector(".display").innerText="";clickedOp=elem.target.innerText;document.querySelector(".operator").innerText=clickedOp;
+   
+    
+}));
+
+
+document.querySelector(".ac").addEventListener("click",elem=>{
+   clearAll();
+});
+
+document.querySelector("body").addEventListener("keydown",elem=>{
+console.log(elem);
+switch (elem.key){
+    case "1":document.querySelector("#one").click();break;
+    case "2":document.querySelector("#two").click();break;
+    case "3":document.querySelector("#three").click();break;
+    case "4":document.querySelector("#four").click();break;
+    case "5":document.querySelector("#five").click();break;
+    case "6":document.querySelector("#six").click();break;
+    case "7":document.querySelector("#seven").click();break;
+    case "8":document.querySelector("#eight").click();break;
+    case "9":document.querySelector("#nine").click();break;
+    case "10":document.querySelector("#ten").click();break;
+}
+});
